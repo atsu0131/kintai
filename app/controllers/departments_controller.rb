26296f8c17a160f4dd6a8d9d0133105efeedf781
atsu0131 <atsushi.isai131@gmail.com>
2018-11-16 20:17:25 +0900

    部署作成

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,4 +2,23 @@ class DepartmentsController < ApplicationController
   def index
     @departments = Department.all
   end
+
+  def new
+    @department = Department.new
+  end
+
+  def create
+    @department = Department.new(department_params)
+    if @department.save
+      redirect_to departments_url, notice: "作成しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def department_params
+    params.require(:department).permit(:name, :email, :position, :active)
+  end
 end
