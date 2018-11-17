e5de143efe454d8a4a59997cfce468196c41fda2
atsu0131 <atsushi.isai131@gmail.com>
2018-11-17 23:33:11 +0900

    アソシエーション修正

--- db/migrate/20181117054824_create_timecards.rb
+++ db/migrate/20181117054824_create_timecards.rb
@@ -1,9 +1,9 @@
 class CreateTimecards < ActiveRecord::Migration[5.2]
   def change
     create_table :timecards do |t|
-      t.references :user, foreign_key: true
+      t.references :user, foreign_key: true, null: false
       t.datetime :start_at, null: false
-      t.datetime :end_at, null: false
+      t.datetime :end_at
       t.timestamps
     end
   end
