929f9826a06ea0a54e9282c13903457bccafaa5a
atsu0131 <atsushi.isai131@gmail.com>
2018-11-18 18:28:18 +0900

    変数名修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   belongs_to :department
   has_many :timecards, dependent: :destroy
   has_many :approvers, dependent: :destroy
-  has_many :approver_users, through: :approves, source: :department
+  has_many :approver_departments, through: :approves, source: :department
 
   enum position: %i{member manager}
 end
