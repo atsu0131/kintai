8972f4ece8bd973e2b076d7ff82592ad58ae683f
atsu0131 <atsushi.isai131@gmail.com>
2018-11-21 16:42:54 +0900

    routesの整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root to: "departments#index"
 
-  resources :departments do
+  resources :departments, only: %i[index new create edit update destroy] do
     resources :users, shallow: true
+  end
+
+  resources :users do
+    resources :timecard, only: %i[index new create edit update destroy]
   end
 end
